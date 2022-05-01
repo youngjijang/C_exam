6cf19dd4bd072fb3df645098c8eeebc7236930dd
Ubuntu <ubuntu@ip-172-31-32-155.ap-northeast-2.compute.internal>
2022-05-01 12:32:45 +0000

    0501

---
+++ linked_list_queue.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*노드 구조체*/
+typedef struct node
+{
+    int num;
+    struct node *p_next;
+
+}NODE;
+
+/*큐 자료구조에 데이터 추가*/
+void append(NODE **p_head, int num){ //tail 노드를 받아서 순회없이 바로 접근하여 추가
+    
+    NODE *p_tail = *p_head;
+    while (NULL != p_tail->p_next)  p_tail = p_tail->p_next;
+    
+    NODE *newNode = (NODE*)malloc(sizeof(NODE));
+    newNode->num = num;
+    newNode->p_next = NULL;
+
+    p_tail->p_next = newNode;
+}
+
+/* leftpop 큐 자료구조 가장 오랜된 데이터를 반환하고 삭제*/
+int leftpop(NODE *p_head){
+    // if(p_head == NULL)
+    //     return 0;
+    
+    NODE *tmp = p_head->p_next;
+    int result = tmp->num;
+    p_head->p_next = tmp->p_next;
+    free(tmp);
+
+    return result;
+}
+
+int main(){
+    NODE *p_head = (NODE*)malloc(sizeof(NODE));
+    p_head->p_next = NULL;
+
+    append(&p_head,100);
+    append(&p_head,200);
+    append(&p_head,300);
+
+    // NODE *p = p_head; // headnode부터 시작
+    // while(p != NULL){
+    //     printf("%d\n",p->num);
+    //     p = p->p_next;
+    // }
+
+    int result = leftpop(p_head);
+    printf("%d\n",result);
+
+    
+
+}
